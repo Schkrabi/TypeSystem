9d4e47955a29159868f82747a57db80c9ab13902
Radomir Skrabal <skrabal_Radomir@centrum.cz>
2020-12-28 10:05:25 +0100

    non-java fixes

--- clojure/lang-pstr.clj
+++ clojure/lang-pstr.clj
@@ -1,14 +1,14 @@
-(defrecord lang-type-atom [name representation])
-(defrecord lang-type-arrow [arg-type return-type])
+;; (defrecord lang-type-atom [name representation])
+;; (defrecord lang-type-arrow [arg-type return-type])
 
-(def test-int (with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")}))
-(def test-string (with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")}))
-(def test-bool-t (with-meta [true] {:lang-type (lang-type-atom. "Bool" "Native")}))
-(def test-bool-f (with-meta [false] {:lang-type (lang-type-atom. "Bool" "Native")}))
-(def test-tuple-int (with-meta [(with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")}) (with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")})] {:lang-type [(lang-type-atom. "Int" "Native") (lang-type-atom. "Int" "Native")]}))
-(def test-tuple-str (with-meta [(with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")}) (with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")})] {:lang-type [(lang-type-atom. "String" "Native") (lang-type-atom. "String    " "Native")]}))
-(def test-fun (with-meta [(with-meta (fn [x] x) {:lang-type (lang-type-arrow. ["SYSGENNAMEv"] "SYSGENNAMEv")})] {:lang-type (lang-type-arrow. ["SYSGENNAMEv"] "SYSGENNAMEv")}))
-(def test-composite (with-meta [(with-meta ["XLII"] {:lang-type (lang-type-atom. "String" "Native")})] {:lang-type (lang-type-atom. "Int" "Roman")}))
+;; (def test-int (with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")}))
+;; (def test-string (with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")}))
+;; (def test-bool-t (with-meta [true] {:lang-type (lang-type-atom. "Bool" "Native")}))
+;; (def test-bool-f (with-meta [false] {:lang-type (lang-type-atom. "Bool" "Native")}))
+;; (def test-tuple-int (with-meta [(with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")}) (with-meta [42] {:lang-type (lang-type-atom. "Int" "Native")})] {:lang-type [(lang-type-atom. "Int" "Native") (lang-type-atom. "Int" "Native")]}))
+;; (def test-tuple-str (with-meta [(with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")}) (with-meta ["foo"] {:lang-type (lang-type-atom. "String" "Native")})] {:lang-type [(lang-type-atom. "String" "Native") (lang-type-atom. "String    " "Native")]}))
+;; (def test-fun (with-meta [(with-meta (fn [x] x) {:lang-type (lang-type-arrow. ["SYSGENNAMEv"] "SYSGENNAMEv")})] {:lang-type (lang-type-arrow. ["SYSGENNAMEv"] "SYSGENNAMEv")}))
+;; (def test-composite (with-meta [(with-meta ["XLII"] {:lang-type (lang-type-atom. "String" "Native")})] {:lang-type (lang-type-atom. "Int" "Roman")}))
 
 (def lang-pstr
     (fn [exp]
@@ -16,16 +16,16 @@
                     (let [type (:lang-type (meta exp))]
                         (cond
                             (or 
-                                (= type (lang-type-atom. "Int" "Native")) 
-                                (= type (lang-type-atom. "String" "Native")) 
-                                (= type (lang-type-atom. "Double" "Native"))
-                                (= type (lang-type-atom. "Bool" "Native"))) (if 
-                                                                                (= level 0) 
-                                                                                (pr-str (get exp 0)) 
-                                                                                (get exp 0))
-                            (= type []) []
-                            (instance? lang-type-atom type) (lang-pstr-aux (get exp 0) level)
-                            (instance? clojure.lang.PersistentVector type) 
+                                (= type velka.lang.types.TypeAtom/TypeIntNative)
+                                (= type velka.lang.types.TypeAtom./TypeStringNative) 
+                                (= type velka.lang.types.TypeAtom/TypeDoubleNative) 
+                                (= type velka.lang.types.TypeAtom/TypeBoolNative)) 
+                                    (if (= level 0) 
+                                        (pr-str (get exp 0)) 
+                                        (get exp 0))
+                            (= type velka.lang.types.TypeTuple/EMPTY_TUPLE) []
+                            (instance? velka.lang.types.TypeAtom type) (lang-pstr-aux (get exp 0) level)
+                            (instance? velka.lang.types.TypeTuple type) 
                             (if 
                                 (= level 0) 
                                 (pr-str (vec (map (fn [x] (lang-pstr-aux x (+ level 1))) exp)))
