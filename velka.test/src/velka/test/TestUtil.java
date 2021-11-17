09dcb6f90544dc19709e7984b4a4fa7eb4dbcec9
Radomir Skrabal <skrabal_Radomir@centrum.cz>
2021-11-17 13:58:18 +0100

    Removed unused import

--- velka.test/src/velka/test/TestUtil.java
+++ velka.test/src/velka/test/TestUtil.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Vector;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
